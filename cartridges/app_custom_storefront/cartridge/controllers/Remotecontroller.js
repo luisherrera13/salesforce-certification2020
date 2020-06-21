764bc0d20567ffe1fc465a0c903eb34f17d23864
luisherrera13 <jlhcorp2@hotmail.com>
2020-06-20 19:01:27 -0600

    isml practice for second round-remoteinclude added

--- cartridges/app_custom_storefront/cartridge/controllers/Remotecontroller.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Remotecontroller.js
@@ -6,9 +6,9 @@ var server = require('server');
 
 server.get('Show', function(req, res, next) {
 //server.append('Show', function(req, res, next) {
-   res.json({ value: 'this json response comes from a Hellopeople.js controller called from a ismodule tag'});
-  //  res.render('exercises/exerciseIsml', { value: 'Hello World' });
-  //res.render("/exercises/exerciseIsml");
+  // res.json({ value: 'this json response comes from a Hellopeople.js controller called from a ismodule tag'});
+  //  res.render('components/footer/exerciseFooter', { value: 'Hello World' });
+  res.render("components/remotehello.isml");
  // res.setViewData({ value: 'Hello Commerce Cloud' });
     next();
 });
