df1c07502740f128589512b076f5cf1960c7ca7b
luisherrera13 <jlhcorp2@hotmail.com>
2020-06-20 19:06:24 -0600

    isml practice for second round-remoteinclude added

--- cartridges/app_custom_storefront/cartridge/controllers/Jsonrequest.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Jsonrequest.js
@@ -6,7 +6,7 @@ var server = require('server');
 
 server.get('Show', function(req, res, next) {
 //server.append('Show', function(req, res, next) {
-   res.json({ value: 'this json response comes from a Jsonrequest.js controller called from a ismodule tag'});
+   res.json({ value: 'this json response comes from a Jsonrequest.js controller called from a component tag'});
   //  res.render('components/footer/exerciseFooter', { value: 'Hello World' });
  // res.render("/exercises/exerciseIsml");
  // res.setViewData({ value: 'Hello Commerce Cloud' });
